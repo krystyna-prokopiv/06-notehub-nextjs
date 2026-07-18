6a9f6acd39c59ada8c3f4ce36e397de83b55e782
Krystyna Prokopiv <krisrinaars237@gmail.com>
2026-07-18 17:00:04 +0300

    rename client

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,7 +4,7 @@ import {
   dehydrate,
 } from "@tanstack/react-query";
 import { fetchNotes } from '@/lib/api'
-import NotesClient from "./Notes.client/Notes.client";
+import NotesClient from "./Notes.client";
 
 export default async function NotesPage() {
   const queryClient = new QueryClient();
